e5ffb19c1b6e731ef098a82d7d4a180f2fad7c67
Eric Jiang <erjiang@indiana.edu>
2011-04-16 20:32:00 -0400

    Added more documentation to the javascript FFI

--- src/lib/javascript.js
+++ src/lib/javascript.js
@@ -3,11 +3,26 @@
  * Implements the important native JS functions for FoxScheme
  * (i.e. things that cannot be written in Scheme)
  */
-/*
+/* for compatibility with dependency grapher:
 var FoxScheme.lib.JavaScript = function() {
 */
 (function(defun) {
 
+//
+// js:eval
+// -------
+//
+// js:eval simply uses JavaScript's `eval` function to evaluate the given
+// text. It expects to receive a FoxScheme.String as its input.
+//
+// The result of js:eval should be the result of the JavaScript eval, which is
+// usually the last expression in the eval'd code. For example,
+//
+//     (js:eval "2+2;4+4") => 8
+//
+// Notice that if the result of evaluating the code is `undefined`,
+// FoxScheme.nothing (i.e. `#<void>`) is returned instead.
+//
 defun("js:eval", 1, 1,
     function() {
         var expr = arguments[0];
@@ -23,6 +38,35 @@ defun("js:eval", 1, 1,
         return retval
     })
 
+//
+// js:procedure
+// ------------
+//
+// js:procedure creates a FoxScheme procedure from a JavaScript procedure.
+// Essentially, the JavaScript procedure is wrapped in FoxScheme code that
+// allows it to be used as a regular Scheme procedure would be.
+// 
+// To use it, simply pass js:procedure a string that represents normal
+// JavaScript code:
+//
+//     ((js:procedure "function (a, b) { return Math.sqrt(a*a + b*b) }") 3 4)
+//     => 5
+//
+// There is no lexical scope between the JavaScript code and FoxScheme code;
+// variables share two different namespaces, and consequently, the following
+// won't work:
+//
+//     (let ((x 3))
+//       ((js:procedure "function (y) { return x * y; }')))
+//     => ReferenceError: x is not defined (varies by JS implementation)
+//
+// Please remember that you must account for FoxScheme data types when passing
+// data back and forth. You must, for example, convert a JS string to a
+// FoxScheme.String before you return it, as all strings in FoxScheme are of
+// type FoxScheme.String. Likewise, any string that is passed in from FoxScheme
+// is a FoxScheme.String and must be converted to use it like JavaScript. This
+// may change in the future.
+//
 defun("js:procedure", 1, 1,
     function() {
         var expr = arguments[0];
