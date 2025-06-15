99a3ba62c30efc9c01380e3671762a03a9254e9f
Eric Jiang <321497+erjiang@users.noreply.github.com>
2025-06-14 21:21:09 -0700

    feat(node): implement load function

--- src/shim/node.ts
+++ src/shim/node.ts
@@ -1,7 +1,33 @@
 import repl from 'repl';
+import fs from 'fs';
 import * as FoxScheme from '../foxscheme';
+import {defun} from '../system/native';
+import String from '../system/string';
+import {Error as SchemeError} from '../system/error';
+import nothing from '../system/nothing';
 
 const interp = new FoxScheme.Interpreter();
+
+// Provide a Scheme-level (load) implementation for Node
+defun("load", 1, 2, function(filename, evalproc) {
+  if (!(filename instanceof String))
+    throw new SchemeError("Must specify a filename string to load from", "load");
+  if (evalproc === undefined)
+    evalproc = this.eval;
+  const path = filename.getValue();
+  let data: string;
+  try {
+    data = fs.readFileSync(path, 'utf8');
+  } catch (e) {
+    throw new SchemeError("Failed to read file: " + (e as any).message, "load");
+  }
+  const p = new FoxScheme.Parser(data);
+  let o: any;
+  while ((o = p.nextObject()) !== FoxScheme.Parser.EOS) {
+    evalproc.apply(this, [o]);
+  }
+  return nothing;
+});
 
 let runWithInterpreter: repl.REPLEval;
 runWithInterpreter = function(cmd, context, filename, callback) {
@@ -20,8 +46,12 @@ myWriter = function(obj) {
   return "" + obj;
 }
 
-repl.start({
-  prompt: '> ',
-  eval: runWithInterpreter,
-  writer: myWriter
-})
+if (require.main === module) {
+  repl.start({
+    prompt: '> ',
+    eval: runWithInterpreter,
+    writer: myWriter
+  });
+}
+
+export default FoxScheme;
